0cf0cc3096d4c00d048503ab42022fd96d715014
KiaraBeln <kiarbelltorales@gmail.com>
2025-07-06 14:01:12 -0300

    Clase
    1

---
+++ Clase 1/Clase1/src/main/java/clase1/demo/HolaMundo.java
@@ -0,0 +1,10 @@
+package clase1.demo;
+
+public class HolaMundo {
+public static void main(String[] args) {
+    //Tipo Comentario de linea
+    System.out.println("¡Hola, Mundo!");
+   /* Es un comentario en bloque */
+    System.out.println("¡Hola, Mundo! desde la clase 1!");
+  }
+}
